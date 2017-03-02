17a43a640e032b7234a70d88fbdf56065b8564c3
Kunal Bhuwalka <kunal.bhuwalka@hotmail.com>
2017-03-03 03:45:50 +0530

    fix: empty response bug
    Added a dependency to npm module -> request. All requests to udacity
    servers have been refactored to use this module.
    
    Fixes #1

--- src/routes/ajax.js
+++ src/routes/ajax.js
@@ -3,6 +3,7 @@
 var https = require('https');
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyNDUzNiwiZXhwIjoxNDkwODQxOTU1LCJ0b2tlbl90eXBlIjoiYXBpIn0.plRlBl-rX4nMFg8kV7yHOnU76m_Z2GzSV_KGhL04vY4";
 
@@ -30,7 +31,7 @@ router.get('/position', function(req,res){
     console.dir(data);
     console.log("\n\n");
     res.send(data);
-  })
+  });
 });
 
 router.get('/new', function(req,res){
@@ -40,7 +41,7 @@ router.get('/new', function(req,res){
     console.dir(data);
     console.log("\n\n");
     res.send(data);
-  })
+  });
 });
 
 router.get('/request', function(req, res){
@@ -51,72 +52,53 @@ router.get('/request', function(req, res){
     console.dir(data);
     console.log("\n\n");
     res.send(data);
-  })
+  });
 });
 
 function getRequest(requestId, callback){
   var options = {
-    host: 'review-api.udacity.com',
-    path: '/api/v1/submission_requests/'+requestId,
-    method: 'GET',
+    url: 'https://review-api.udacity.com/api/v1/submission_requests/'+requestId,
     headers: header
   };
-  httpRequest(options, "", callback);
+  httpRequest(options, callback);
 }
 
 function getNew(callback){
   var data = '{"projects":[{"project_id":52,"language":"en-us"}]}';
-
-  var postHeaders = header;
-  postHeaders['Content-Type'] = 'application/json';
-  postHeaders['Content-Length'] = Buffer.byteLength(data);
-
   var options = {
-    host: 'review-api.udacity.com',
-    path: '/api/v1/submission_requests/',
+    url: 'https://review-api.udacity.com/api/v1/submission_requests/',
     method: 'POST',
-    headers: postHeaders
+    headers: header,
+    body: data
   };
 
-  httpRequest(options, data, callback);
+  httpRequest(options, callback);
 }
 
 function getPosition(requestId, callback){
   var options = {
-    host: 'review-api.udacity.com',
-    path: '/api/v1/submission_requests/'+requestId+'/waits',
-    method: 'GET',
+    url: 'https://review-api.udacity.com/api/v1/submission_requests/'+requestId+'/waits',
     headers: header
   };
-  httpRequest(options, "", callback);
+  httpRequest(options, callback);
 }
 
 
 function refreshProject(requestId, callback){
   var options = {
-    host: 'review-api.udacity.com',
-    path: '/api/v1/submission_requests/'+requestId+'/refresh',
+    url: 'https://review-api.udacity.com/api/v1/submission_requests/'+requestId+'/refresh',
     method: 'PUT',
     headers: header
   };
-  httpRequest(options, "", callback);
+  httpRequest(options, callback);
 }
 
-function httpRequest(options, body, callback){
-  var data = '';
-  var request = https.request(options, function(response){
-    response.setEncoding('utf8');
-    response.on('data', function(chunk){
-        data+=chunk;
-    });
-    response.on("end", function(){
-      callback(data);
-    });
-  });
-  if(body != ''){
-    request.write(body);
-  }
-  request.end();
+function httpRequest(options, callback){
+  var onResponse = function(error, response, body){
+    if(error) console.log(error);
+    callback(body);
+  };
+  request(options, onResponse);
 }
 
 module.exports = router;
