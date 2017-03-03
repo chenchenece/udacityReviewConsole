e67450691fc653316115c01c04af2ff7a5381188
Kunal Bhuwalka <kunal.bhuwalka@hotmail.com>
2017-03-04 02:52:07 +0530

    feat: Add development server
    A development server can be started from the gulp task dev-serve which
    will restart the server on changes to application file, and refresh the
    browser after building static files.

--- gulpfile.js
+++ gulpfile.js
@@ -7,23 +7,21 @@ var gulp = require('gulp'),
     copy = require('gulp-copy'),
     clean = require('gulp-clean'),
     utils = require('gulp-util'),
-    cleanCSS = require('gulp-clean-css');
+    cleanCSS = require('gulp-clean-css'),
+    nodemon = require('gulp-nodemon');
 
 var browserSync = require('browser-sync').create();
 var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task("dev-serve",['watchFiles'], function(){
-
-  browserSync.init({
-      server: {
-          baseDir: "./src"
-      }
+gulp.task("dev-serve",['watchFiles','nodemon'], function(){
+  browserSync.init(null, {
+          proxy: "http://localhost:3000",
+          port: 3500
   });
-
 });
 
 gulp.task('watchFiles', function(){
-  gulp.watch(['src/assets/css/**'], ['minifyStyles']);
+  gulp.watch(['src/assets/css/**'], ['afterMinifyStyles']);
   gulp.watch(['src/assets/js/**'], ['afterMinifyScripts']);
 });
 
@@ -54,6 +52,11 @@ gulp.task("minifyStyles", function(){
       .pipe(browserSync.stream());
 });
 
+gulp.task('afterMinifyStyles', ['minifyStyles'], function(done){
+  browserSync.reload();
+  done();
+});
+
 gulp.task("copyImages", function(){
   return gulp.src("src/assets/imgs/**")
       .pipe(gulp.dest("src/dist/imgs"));
@@ -62,4 +65,14 @@ gulp.task("copyImages", function(){
 gulp.task("cleanBuild", function(){
   return gulp.src("src/dist", {read: false})
       .pipe(clean());
+});
+
+gulp.task('nodemon', function (cb) {
+    var callbackCalled = false;
+    return nodemon({script: ''}).on('start', function () {
+        if (!callbackCalled) {
+            callbackCalled = true;
+            cb();
+        }
+    });
 });
